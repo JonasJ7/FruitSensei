cbe39c75dea95f7f24ef8fa246a7f0ceca0d23ec
FrederikBottrup <Frederikkbottrup@gmail.com>
2022-01-07 12:01:42 +0100

    Please help with slash

---
+++ Assets/Scripts/ScreenSlice.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSliceTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform sliceTransform;
+    public GameObject rb1;
+
+    public void Start()
+    {
+        StartCoroutine(cool());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            sliceTransform.gameObject.SetActive(true);
+
+        }
+        //rb1.GetComponent<ConstantForce>().force = new Vector3(0, -1f, 0);
+    }
+    IEnumerator cool()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            rb1.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
